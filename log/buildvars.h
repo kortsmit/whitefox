eb73145b225e6f5979bfe44cc3d0721e6ef3e86b
Tim Kortsmit <tim@kortsmit.com>
2018-04-04 22:12:57 -0400

    Update firmware

--- log/buildvars.h
+++ log/buildvars.h
@@ -28,22 +28,22 @@
 // ----- Defines -----
 
 // You can change these to give your code its own name.
-#define STR_MANUFACTURER        L"Kiibohd"
+#define STR_MANUFACTURER        L"Input Club"
 #define STR_PRODUCT             L"Keyboard - WhiteFox PartialMap pjrcUSB full"
 #define STR_SERIAL              L"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX - mk20dx256vlh7"
 
 
 // Strings used in the CLI module
-#define CLI_Revision            "ba3c38bf844d0bea1681387ac8602e4ac1ba5f4c"
-#define CLI_Branch              "master"
+#define CLI_Revision            "0a2b22d25f5bb4d515dd96faff48240dea45ccbc"
+#define CLI_Branch              "secure_disable"
 #define CLI_ModifiedStatus      "Clean"
 #define CLI_ModifiedFiles       "\r\n\t"
 #define CLI_RepoOrigin          "https://github.com/kiibohd/controller.git"
-#define CLI_CommitDate          "2016-10-10 23:15:13 -0700"
-#define CLI_CommitAuthor        "GitHub <noreply@github.com>"
+#define CLI_CommitDate          "2017-10-04 09:38:29 -0700"
+#define CLI_CommitAuthor        "Jacob Alexander <haata@kiibohd.com>"
 #define CLI_Modules             "Scan(WhiteFox) Macro(PartialMap) Output(pjrcUSB) Debug(full)"
-#define CLI_BuildDate           "2017-01-09 01:47:51 +0000"
-#define CLI_BuildOS             "Linux-4.1.5-x86_64-linode61"
+#define CLI_BuildDate           "2018-04-05 02:05:28 +0000"
+#define CLI_BuildOS             "Linux-4.14.12-x86_64-linode92"
 #define CLI_Arch                "arm"
 #define CLI_Chip                "mk20dx256vlh7"
 #define CLI_CPU                 "cortex-m4"
@@ -56,5 +56,5 @@
 // match the INF file.
 #define VENDOR_ID               0x1C11
 #define PRODUCT_ID              0xB04D
-#define BCD_VERSION             479
+#define BCD_VERSION             514
 
